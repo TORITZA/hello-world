0819188663bafc9a19260b4318762975bbe7321b
TORITZA <terratreehive@gmail.com>
2025-12-20 09:54:58 -0500

    application instance with Qt Button widget

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from PyQt6.QtWidgets import QApplication, QWidget
 # Only needed for access to command line arguments:
 import sys
+from PyQt6.QtWidgets import QApplication, QPushButton
 
 # You only need ONE QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for the app.
 # If you know you won't use command line arguments, QApplication([]) works too.
 app = QApplication([sys.argv])
 
-# Create a Qt widget, which will be the window.
-window = QWidget()
+# Create a Qt Button widget, which automatically acts as the window.
+window = QPushButton('Push me!')
 window.show() # IMPORTANT!!!!!!!! Windows are hidden by default.
 
 # Starts the event loop:
