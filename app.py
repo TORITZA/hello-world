687e33e9b79208ecd5788079a2383996fb5d5903
TORITZA <terratreehive@gmail.com>
2025-12-21 07:34:18 -0500

    added a QPushButton signal upon clicking a button

--- app.py
+++ app.py
@@ -1,29 +1,32 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
-from PyQt6.QtCore import QSize, Qt
 
-# Subclass QMainWindow to customize app's main window
-# **Allows the window behavior to be self-contained 
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
 class MainWindow(QMainWindow):
+
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle('My App')
 
-        button = QPushButton('Press me!')
+        button = QPushButton("Press me!")
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked)
 
-        self.setMinimumSize(QSize(400, 300))
-
-    # Set the central widget of the window:
+        # Set central widget of the window: 
         self.setCentralWidget(button)
-        # By default, takes the whole of the window.
+
+    def the_button_was_clicked(self):
+        print("Clicked!")
+
 
 
 app = QApplication([])
 
-# Create a Qt Button widget, which automatically acts as the window.
+# Create a Qt Button widget, which automatically acts as the window: 
 window = MainWindow()
 window.show() # IMPORTANT!!!!!!!! Windows are hidden by default.
 
-# Start the event loop.
+# Start the event loop:
 app.exec()
 
