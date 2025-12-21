4a215641f6fad671d4d11cc1aeab533626871b41
TORITZA <terratreehive@gmail.com>
2025-12-21 11:14:00 -0500

    Update text interface in real-time with user input

--- app.py
+++ app.py
@@ -1,18 +1,6 @@
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
-from random import choice
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QLineEdit, QVBoxLayout
 
-window_titles = [
-    'My App', 
-    'My App', 
-    'Still My App',
-    'Still My App',
-    'What on earth',
-    'What on earth',
-    'This is surprising',
-    'This is surprising',
-    'Something went wrong'
-]
 
 
 class MainWindow(QMainWindow):
@@ -20,31 +8,23 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.n_times_clicked = 0 
-
         self.setWindowTitle('My App')
 
-        self.button = QPushButton("Press me!")
-        self.button.clicked.connect(self.the_button_was_clicked)
+        self.label = QLabel()
 
-        self.windowTitleChanged.connect(self.the_window_title_changed)
+        self.input = QLineEdit()
+        self.input.textChanged.connect(self.label.setText)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.label)
+
+        container = QWidget()
+        container.setLayout(layout)
 
         # Set central widget of the window: 
-        self.setCentralWidget(self.button)
+        self.setCentralWidget(container)
 
-    def the_button_was_clicked(self):
-        print("Clicked.")
-        new_window_title = choice(window_titles)
-        print("Setting title: %s" % new_window_title)
-        self.setWindowTitle(new_window_title)
-
-    def the_window_title_changed(self, window_title):
-        # self.windowTitleChanged passes in current (previously altered)
-        # window title to this function
-        print("Window title changed: %s" % window_title)
-
-        if window_title == 'Something went wrong':
-            self.button.setDisabled(True)
 
 
 app = QApplication([])
