6a51c9d7dd76206420deb145fe51249cc3b4856b
TORITZA <terratreehive@gmail.com>
2025-12-26 08:51:27 -0500

    demos QSpinBox & QDoubleSpinBox configurations and functionality

--- app.py
+++ app.py
@@ -22,40 +22,28 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("QLineEdit Demo")
+        self.setWindowTitle("Q(Double)SpinBox Demo")
         self.setFixedSize(400,300)
         
-        line = QLineEdit()
-        line.setMaxLength(10)
-        line.setPlaceholderText("Enter your text")
+        spin = QSpinBox()
+        # Or: spin = QDoubleSpinBox()
 
-        #line.setReadOnly(True)
+        spin.setMinimum(-9)
+        spin.setMaximum(3)
+        # Or: spin.setRange(-9, 3)
 
-        # Perform input validation (IPv4 address example):
-        #line.setInputMask('000.000.000.000;_')
+        spin.setPrefix("$")
+        spin.setSuffix("c")
+        spin.setSingleStep(3) # Or 3.0 for QDoubleSpinBox
+        spin.valueChanged.connect(self.value_changed)
+        spin.textChanged.connect(self.value_changed_str)
 
-        line.returnPressed.connect(self.return_pressed)
-        line.selectionChanged.connect(self.selection_changed)
-        line.textChanged.connect(self.text_changed)
-        line.textEdited.connect(self.text_edited)
-
-        self.setCentralWidget(line)
+        self.setCentralWidget(spin)
     
-    def return_pressed(self):
-        # User hits Enter:
-        print("Return pressed!")
-        self.centralWidget().setText("BOOM!")
+    def value_changed(self, i):
+        print(i)
 
-    def selection_changed(self):
-        print("Selection changed")
-        print(self.centralWidget().selectedText())
-
-    def text_changed(self, s):
-        print("Text changed...")
-        print(s)
-    
-    def text_edited(self, s):
-        print("Text edited...")
+    def value_changed_str(self ,s):
         print(s)
 
 
