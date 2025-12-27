b4f5ae96cda702616029867d53ba697437da2a31
TORITZA <terratreehive@gmail.com>
2025-12-27 09:57:19 -0500

    added custom Color widget (subclass of QWidget) to layout_colorwidget.py

---
+++ layout_colorwidget.py
@@ -0,0 +1,22 @@
+
+from PyQt6.QtGui import QColor, QPalette
+from PyQt6.QtWidgets import QWidget
+
+
+class Color(QWidget):
+    def __init__(self, color: str):
+        super().__init__()
+        
+        # automatically fills its background w/ window color
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        
+        # change current winow color to new, passed-in QColor:
+        palette.setColor(QPalette.ColorRole.Window, QColor(color))
+
+        # apply new color value back to custom widget:
+        self.setPalette(palette)
+
+
+
