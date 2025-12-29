3b8d436b12ff72cd52e45798a57a659058f9f722
TORITZA <terratreehive@gmail.com>
2025-12-29 09:26:29 -0500

    expanded app's toolbar to include a dog icon and checkbox

--- app.py
+++ app.py
@@ -35,6 +35,19 @@ class MainWindow(QMainWindow):
         button_action.setCheckable(True)
         toolbar.addAction(button_action)
 
+        toolbar.addSeparator()
+
+        button_action2 = QAction(QIcon("animal-dog.png"), "My SECOND button", self)
+        button_action2.setStatusTip("This is my SECOND button!!")
+        button_action2.triggered.connect(self.toolbar_button_clicked)
+        button_action2.setCheckable(True)
+        toolbar.addAction(button_action2)
+
+        toolbar.addSeparator()
+
+        toolbar.addWidget(QLabel("do u like?"))
+        toolbar.addWidget(QCheckBox())
+
         self.setStatusBar(QStatusBar(self))
 
     def toolbar_button_clicked(self, s):
